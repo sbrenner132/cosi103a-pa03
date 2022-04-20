35d64e12635c4b19193415be0c5f3fcc2ae96512
naimulh247 <naimulh247@gmail.com>
2022-04-20 03:04:22 +0000

    Completed 3
    	Changed all instances of time2str to use course.strTimes. Updated '/upsertDB',
    		course.ejs, courselist.ejs, and its routes

--- app.js
+++ app.js
@@ -238,8 +238,10 @@ app.get('/upsertDB',
   async (req,res,next) => {
     //await Course.deleteMany({})
     for (course of courses){
-      const {subject,coursenum,section,term}=course;
+      const {subject,coursenum,section,term,strTimes}=course;
       const num = getNum(coursenum);
+      const strTime = strTimes
+      course.strTimes = strTime
       course.num=num
       course.suffix = coursenum.slice(num.length)
       await Course.findOneAndUpdate({subject,coursenum,section,term},course,{upsert:true})
@@ -257,7 +259,7 @@ app.post('/courses/bySubject',
     const courses = await Course.find({subject:subject,independent_study:false}).sort({term:1,num:1,section:1})
     
     res.locals.courses = courses
-    res.locals.times2str = times2str
+    // res.locals.times2str = times2str
     //res.json(courses)
     res.render('courselist')
   }
@@ -269,7 +271,7 @@ app.get('/courses/show/:courseId',
     const {courseId} = req.params;
     const course = await Course.findOne({_id:courseId})
     res.locals.course = course
-    res.locals.times2str = times2str
+    // res.locals.times2str = times2str
     //res.json(course)
     res.render('course')
   }
@@ -296,7 +298,7 @@ app.post('/courses/byInst',
                .sort({term:1,num:1,section:1})
     //res.json(courses)
     res.locals.courses = courses
-    res.locals.times2str = times2str
+    // res.locals.times2str = times2str
     res.render('courselist')
   }
 )
